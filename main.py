85fabed52f8b7fce29d9b693a1861fbf3937bb8f
max <53710132+newgan@users.noreply.github.com>
2023-06-28 15:00:21 -0400

    translate other players and tts

--- main.py
+++ main.py
@@ -1,71 +1,145 @@
 from telnetlib import Telnet
 from deep_translator import GoogleTranslator
 import time
+from gtts import gTTS
+from gtts import lang
+from pydub import AudioSegment
 
 HOST = "127.0.0.1"
 PORT = 4747
-lang_code = "en"
 net = {}
 
+CSGO_DIR = "C:/Program Files (x86)/Steam/steamapps/common/Counter-Strike Global Offensive/"
+
 commands = [
- "#gtr",
- "#tr",
- "#lang"
+    "#gtr",
+    "#tr",
+    "#lang",
+    "#olang",
+    "#tts",
+    "#ttts",
 ]
 
-def translate(msg, global_chat=False):
+lang_list = GoogleTranslator().get_supported_languages()
+voice_list = lang.tts_langs()
+voice_list = [voice.lower() for voice in voice_list]
+
+
+def translate(msg, lang_code):
     try:
-        new_msg = GoogleTranslator(source='auto', target=lang_code).translate(msg.rstrip())
+        return GoogleTranslator(
+            source='auto', target=lang_code).translate(msg.rstrip())
     except:
         "translation failed"
         return
-    
+
+
+def translate_to_chat(msg, lang_code, global_chat=False):
+    new_msg = translate(msg, lang_code)
+
+    if not new_msg:
+        return
+
     prefix = "say" if global_chat else "say_team"
-    
+
     time.sleep(1)
-    net.write(f'{prefix} "{new_msg}"\n'.encode())  
-   
-def handle_cmd(cmd, msg):
-    if(match == "#tr"):
-        translate(msg)
-    elif(match == "#gtr"):
-        translate(msg, True)
-    elif(match == "#lang"):
-        lang_code = msg.strip()
-                
+    net.write(f'{prefix} "{new_msg}"\n'.encode())
+
+
+def translate_to_console(msg, lang_code, author):
+    new_msg = translate(msg, lang_code)
+
+    if new_msg and new_msg in msg:  # language is the same
+        return
+
+    net.write(f'echo {author}(TRANSLATED): "{new_msg}"\n'.encode())
+
+
+def text_to_speech(msg, lang_code="en", should_translate=False):
+    if should_translate:
+        msg = translate(msg, lang_code)
+
+    if lang_code not in voice_list:
+        lang_code = "en"
+
+    tts = gTTS(msg, lang=lang_code)
+    tts.save(f'{CSGO_DIR}voice_input.mp3')
+
+    sound = AudioSegment.from_mp3(f'{CSGO_DIR}voice_input.mp3')
+    sound.export(f'{CSGO_DIR}voice_input.wav', format="wav")
+
+
+def get_self_name():
+    net.write("name\n".encode())
+
+    while True:
+        line = net.read_until(b"\n").decode()
+        if ('"name"' in line):
+            return line.split('" ( def')[0].split('name" = "')[1]
+
+
 try:
     with Telnet(host=HOST, port=PORT) as net:
         print("connected to console!")
-        
+
         net.write("clear\n".encode())
-        net.write("""                                                                                                                                                                                                                                                         
+        net.write("""
 {0} -----------------------------------------------------;
 {0} ------------- welcome to cs translator! -------------;
 {0} -----------------------------------------------------;
 {0} #gtr - send a translated message to global chat.     ;
 {0} #tr - send a translated message to team chat.        ;
-{0} #lang - change language to translate to.             ;
+{0} #tts - TTS a message in your default language.       ;
+{0} #ttts - TTS a translated message.                    ;
+{0} #lang - change language to translate to (self).      ;
+{0} #olang - change language to translate to (other).    ;
 {0}                                                      ;
 {0}                                                      ;
 {0}                                                      ;
-{0}                                                      ;
-{0}                                                      ;
-{0}                                                      ;
-{0} -----------------------------------------------------;""".format("echo").encode())
-        
+{0} -----------------------------------------------------;\n""".format("echo").encode())
+
+        # workaround to ignore the header message
+        time.sleep(3)
+        net.read_very_eager()
+
+        self = get_self_name()
+
+        self_lang_code = "en"
+        other_lang_code = "en"
+
+        net.write("voice_inputfromfile 1\n".encode())
+
         while True:
-            line = net.read_until(b"\n").decode()        
-            match = [cmd for cmd in commands if(cmd in line)]
-            
-            if not match:
+            try:
+                line = net.read_until(b"\n").decode()
+            except:
                 continue
-            
-            match = match[0]
-            
+
+            match = next((cmd for cmd in commands if (cmd in line)), None)
             author = line.split(":", 1)[0]
-            msg = line.split(match, 1)[1]
-            
-            handle_cmd(match, msg)                                               
+
+            other_player_chat = " : " in line
+            ignore = self in line
+
+            if other_player_chat and not ignore:
+                translate_to_console(line.split(
+                    " : ")[1], other_lang_code, author)
+            elif match:
+                msg = line.split(match, 1)[1]
+
+                if match == "#tr":
+                    translate_to_chat(msg, self_lang_code)
+                elif match == "#gtr":
+                    translate_to_chat(msg, self_lang_code, True)
+                elif match == "#tts":
+                    text_to_speech(msg)
+                elif match == "#ttts":
+                    text_to_speech(msg, self_lang_code, True)
+                elif match == "#lang":
+                    self_lang_code = msg.strip()
+                elif match == "#olang":
+                    other_lang_code = msg.strip()
 except ConnectionRefusedError:
-    print(f'please start csgo with the following launch option: -netconport {PORT}')
+    print(
+        f'please start csgo with the following launch option: -netconport {PORT}')
     quit()
